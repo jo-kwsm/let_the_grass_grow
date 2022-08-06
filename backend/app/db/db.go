a854d97d7b4713c458d05169dff644ec3b73caca
snowyuki <y-maeda.1031@keio.jp>
2022-08-06 11:18:54 +0900

    add func for db connection and a model

---
+++ backend/app/db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func Init() {
+	dbconf := fmt.Sprintf("%s:%s@db:3306/%s?charset=utf8", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_DATABASE"))
+
+	var err error
+	db, err = gorm.Open(mysql.Open(dbconf), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Db connection error: ", err)
+	}
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
